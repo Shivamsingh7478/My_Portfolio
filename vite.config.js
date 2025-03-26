b1ee63782aaff4636b8958224e7a67e77c7cb0ea
Shivamsingh7478 <shivamsingh2681@gmail.com>
2025-03-26 14:14:55 +0530

    adding project file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/My_portfolio/',  // Add this line for GitHub Pages deployment
+  build: {
+    outDir: 'dist',  // Ensure build output goes to 'dist'
+  },
+});
